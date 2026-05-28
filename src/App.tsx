07dd680c132fcf13affdf3839562fb330e71b32a
Marvel123g <marvellousolorunwa23@gmail.com>
2026-05-28 16:57:06 +0100

    Fixed the width and height of the background

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import HomeContent from './components/HomeContent'
 import Nav from './components/Nav'
 
 function App() {
-  
+  // bg-[url(./assets/background.gif)] bg-cover bg-center
 
   return (
-    <div className="bg-[url(./assets/background.gif)] bg-cover bg-center min-h-screen font-[RobotoMono] p-5">
-      <Nav/>
-      <HomeContent/>
+    <div className=" font-[RobotoMono] relative min-h-screen flex flex-col justify-center">
+      <img src="./background.gif" alt="background image" className='absolute h-full w-full -z-50'/>
+      <div className='p-5 z-50'>
+        <Nav/>
+        <HomeContent/>
+      </div>
     </div>
   )
 }

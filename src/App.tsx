4b9f43a1f7547cc3116be1d21136af2f9b01307a
Marvel123g <marvellousolorunwa23@gmail.com>
2026-05-28 19:31:07 +0100

    Changed the padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Nav from './components/Nav'
 function App() {
 
   return (
-    <div className=" font-[RobotoMono] relative min-h-screen">
+    <div className=" font-[RobotoMono] relative min-h-screen flex flex-col items-center justify-center">
       <img src="./background.gif" alt="background image" className='absolute h-full w-full -z-50' loading='eager'/>
-      <div className='sm:p-5 z-50 flex flex-col min-h-screen'>
+      <div className='sm:p-5 z-50 min-h-screen'>
         {/* Using the Nav component that was imported */}
         <Nav/>
 
         {/* Using the HomeContent component that was imported */}
-        <div className='grow flex items-center justify-center max-w-350'>
+        <div className='grow pt-9 max-w-350'>
           <HomeContent/>
         </div>
       </div>

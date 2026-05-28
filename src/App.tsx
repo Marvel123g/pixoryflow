96dc32a85f2090a03862eb99fa4024da7bf95934
Marvel123g <marvellousolorunwa23@gmail.com>
2026-05-28 17:35:16 +0100

    Added more spacing to the content

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Nav from './components/Nav'
 function App() {
 
   return (
-    <div className=" font-[RobotoMono] relative flex flex-col justify-between">
+    <div className=" font-[RobotoMono] relative min-h-screen">
       <img src="./background.gif" alt="background image" className='absolute h-full w-full -z-50' loading='eager'/>
-      <div className='p-5 z-50'>
+      <div className='p-5 z-50 flex flex-col gap-10 justify-between'>
         <Nav/>
         <HomeContent/>
-      </div>                                                                                                                      
+      </div>
     </div>
   )
 }
